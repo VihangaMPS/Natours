c574f8c39a3162808a6dee6978b559dcc186da6a
MPS Vihanga <>
2024-05-24 00:54:09 +0530

    responding to url params

--- app.js
+++ app.js
@@ -4,14 +4,37 @@ const fs = require("fs");
 const app = express();
 app.use(express.json()); // Middleware
 
+
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
 app.get('/api/v1/tours', (req, res) => {
     res.status(200)
         .json({
             status: 'success',
+            results: tours.length,
             data: {
                 tours: tours
+            }
+        });
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+    // console.log(req.params); // request paramId gives output as a String
+    const id = req.params.id * 1; // converting paramId(String) to a int
+    const tour = tours.find(element => element.id === id);
+
+    if (!tour) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid id'
+        })
+    }
+
+    res.status(200)
+        .json({
+            status: 'success',
+            data: {
+                tour: tour
             }
         });
 });
@@ -31,6 +54,9 @@ app.post('/api/v1/tours', (req, res) => {
     });
 });
 
+
+
+// ====================== SERVER ======================
 const port = 3000;
 app.listen(port, () => {
     console.log(`App running on port ${port}...`)
