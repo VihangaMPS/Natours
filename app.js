63725f5af7939e3982ac22c0001b44133f416795
MPS Vihanga <>
2024-05-24 13:47:18 +0530

    creating & mounting multiple routers

--- app.js
+++ app.js
@@ -107,24 +107,28 @@ const getAllUsers = (req, res) => {
         message: 'This route is not yet defined'
     });
 };
+
 const getUser = (req, res) => {
     res.status(500).json({
         status: 'error',
         message: 'This route is not yet defined'
     });
 };
+
 const createUser = (req, res) => {
     res.status(500).json({
         status: 'error',
         message: 'This route is not yet defined'
     });
 };
+
 const updateUser = (req, res) => {
     res.status(500).json({
         status: 'error',
         message: 'This route is not yet defined'
     });
 };
+
 const deleteUser = (req, res) => {
     res.status(500).json({
         status: 'error',
@@ -133,10 +137,15 @@ const deleteUser = (req, res) => {
 };
 
 // ========== Routes  ==============
-app.route('/api/v1/tours').get(getAllTours).post(createTour);
-app.route('/api/v1/tours/:id').get(getTour).patch(updateTour).delete(deleteTour);
-app.route('/api/v1/users').get(getAllUsers).post(createUser);
-app.route('/api/v1/users/:id').get(getUser).patch(updateUser).delete(deleteUser)
+const tourRouter = express.Router();
+const userRouter = express.Router();
+app.use('/api/v1/tours', tourRouter); // creating another router for Tour using middleware
+app.use('/api/v1/users', userRouter);
+
+tourRouter.route('/').get(getAllTours).post(createTour);
+tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
+userRouter.route('/').get(getAllUsers).post(createUser);
+userRouter.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
 // ====================== SERVER ======================
 const port = 3000;
