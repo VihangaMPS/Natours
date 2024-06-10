ed7583580370927d9cb0dfdfe0c4cfa0917207fa
MPS Vihanga <>
2024-06-10 12:30:24 +0530

    Applying Data Sanitization

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -12,9 +14,13 @@ const app = express(); // Main Router
 
 // ================== Middleware =====================
     // ----------- Middleware to send the req json body when we create(POST) -----------
-app.use(express.json({
-    limit: '10kb'
-}));
+app.use(express.json({ limit: '15kb' }));
+
+    // ------------ Data Sanitization against NoSQL query injection { "$gt": "" } ------------
+app.use(mongoSanitize()); // filtered out operators
+
+    // ------------ Data Sanitization against XSS ------------
+app.use(xss());
 
     // ---------- Setting Security HTTP Headers -----------
 app.use(helmet())
